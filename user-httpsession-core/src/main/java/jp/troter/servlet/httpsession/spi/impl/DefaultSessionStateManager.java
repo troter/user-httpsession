e868e81a2d6bc5743d34cd9ae3aafe1bb68f1091
troter <trot.thunder@gmail.com>
2011-05-26 20:42:16 +0900

    add log

--- user-httpsession-core/src/main/java/jp/troter/servlet/httpsession/spi/impl/DefaultSessionStateManager.java
+++ user-httpsession-core/src/main/java/jp/troter/servlet/httpsession/spi/impl/DefaultSessionStateManager.java
@@ -1,21 +1,32 @@
 package jp.troter.servlet.httpsession.spi.impl;
 
+import java.util.HashMap;
+
 import jp.troter.servlet.httpsession.spi.SessionStateManager;
+import jp.troter.servlet.httpsession.state.DefaultSessionState;
 import jp.troter.servlet.httpsession.state.SessionState;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DefaultSessionStateManager extends SessionStateManager {
 
+    private static Logger log = LoggerFactory.getLogger(DefaultSessionStateManager.class);
+
     @Override
     public void updateState(String sessionId, SessionState sessionState) {
+        log.error("DefaultSessionStateManager.updateState is stub.");
     }
 
     @Override
     public void removeState(String sessionId) {
+        log.error("DefaultSessionStateManager.removeState is stub.");
     }
 
     @Override
     public SessionState loadState(String sessionId) {
-        return null;
+        log.error("DefaultSessionStateManager.loadState is stub.");
+        return new DefaultSessionState(new HashMap<String, Object>());
     }
 
 }
