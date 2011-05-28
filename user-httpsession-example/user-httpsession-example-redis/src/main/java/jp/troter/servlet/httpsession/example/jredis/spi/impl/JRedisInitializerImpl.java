c10c095a347d9a3027df25ce12c81a1a7e8cabe4
troter <trot.thunder@gmail.com>
2011-05-28 22:44:44 +0900

    example redis

---
+++ user-httpsession-example/user-httpsession-example-redis/src/main/java/jp/troter/servlet/httpsession/example/jredis/spi/impl/JRedisInitializerImpl.java
@@ -0,0 +1,32 @@
+package jp.troter.servlet.httpsession.example.jredis.spi.impl;
+
+import java.util.concurrent.TimeUnit;
+
+import jp.troter.servlet.httpsession.spi.JRedisInitializer;
+
+import org.jredis.JRedis;
+import org.jredis.RedisException;
+import org.jredis.ri.alphazero.JRedisClient;
+
+public class JRedisInitializerImpl extends JRedisInitializer {
+
+    JRedis jredis;
+
+    @Override
+    public JRedis getJRedis() {
+        if (jredis == null) {
+            try {
+                jredis = new JRedisClient("localhost", 6379);
+                jredis.ping();
+            } catch (RedisException e) {
+            }
+        }
+        return jredis;
+    }
+
+    @Override
+    public int getSessionTimeout() {
+        return Long.valueOf(TimeUnit.HOURS.toSeconds(1L)).intValue();
+    }
+
+}
